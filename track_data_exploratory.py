ec47a1e6ab8abc3639db47bbcd888f7a46366420
lqmeyers <lqmeyers@gmail.com>
2022-11-20 15:39:31 -0800

    cleaning + doc

--- track_data_exploratory.py
+++ track_data_exploratory.py
@@ -9,7 +9,7 @@ filename = r"/home/lqmeyers/SLEAP_files/h5_files/validation_22_22_6.000_fixed2x6
 
 
 
-''' #for writing coords per track 
+ #for writing coords per track 
 def parseTrackData(file):
   with h5py.File(file,'r') as f:
     #dset_names = list(f.keys())
@@ -21,30 +21,53 @@ def parseTrackData(file):
 data = parseTrackData(filename)
 print(data.shape)
 
-targetTrack = 6
+targetTrack = 26
 
 file = open('track_coords.txt','w') #write to txt file to see actual coords per track 
+file.write('Coords for track # '+str(targetTrack)+u'\n')
 for f in range(len(data[targetTrack])):
   file.write('frame '+str(f)+' coords = '+str(data[targetTrack,f,3,:])+u'\n')
 file.close()
 print('written')
-'''
+
+"""
 
 def parseTrackScores(file):
   '''gets track scores from h5 file'''
   with h5py.File(file,'r') as f:
     dset_names = list(f.keys())
     print(dset_names)
-    scores = f['instance_scores'][:].T#'tracking_scores'][:].T
+    scores = f['tracking_scores'][:].T#'instance_scores'][:].T
   return scores
 
 data = parseTrackScores(filename)
 print(data.shape)
+"""
 
-#"""
+"""
 file = open('instance_scores.txt','w')
 for f in range(len(data)):
   file.write('frame '+str(f)+' score = '+str(data[f])+u'\n')
 file.close()
 print('written')
-#"""
+"""
+
+"""-----------------------------------------
+#make a histogram of scores 
+
+allScores = []
+
+for i in range(len(data)):
+  for val in data[i]:
+   if val != np.nan:
+    allScores.append(val)
+
+from matplotlib import pyplot as plt 
+
+fig, ax = plt.subplots() 
+ax.hist(allScores,30)
+
+plt.show(block=False) #cant find much on what block does but necessarty tpo close 
+plt.pause(30) #keeps viewer open 
+plt.close() #closes viewer
+"""
