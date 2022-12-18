1045f88274982c536f5d04b15da2819f58633cd3
lqmeyers <lqmeyers@gmail.com>
2022-12-18 03:48:42 -0800

    display photo from cmd with cv2

---
+++ photoDisplay.py
@@ -0,0 +1,18 @@
+import sys 
+import cv2
+
+sys.path.insert(0, '/mnt/c/Users/lqmey/OneDrive//Desktop/Bee_Visit_Count/')
+
+
+imgFile = sys.argv[1]
+
+img = cv2.imread(imgFile)
+cv2.imshow('display',img)
+#cv2.waitKey(3000)
+#cv2.destroyAllWindows()
+
+sys.stdout = open(1, 'w')
+
+
+
+
